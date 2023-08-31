93be55aac7f424ede31e997b5c310ddade87c224
BearHumanS <bye0651@naver.com>
2023-08-31 16:21:37 +0900

    chore: axios 라이브러리 추가

--- src/components/modals/initail-modal.tsx
+++ src/components/modals/initail-modal.tsx
@@ -65,11 +65,11 @@ export const InitialModal = () => {
         <DialogContent className="bg-white text-black p-0 overflow-hidden">
           <DialogHeader className="pt-8 px-6">
             <DialogTitle className="text-2xl text-center font-bold">
-              Customize your server
+              자신만의 서버를 만들어보세요.
             </DialogTitle>
             <DialogDescription className="text-center text-zinc-500">
-              give your server a personaility with a name and an image. you can
-              alsways change it later.
+              서버에 이름과 이미지를 통해 개성을 부여하세요. 나중에 언제든지
+              변경할 수 있습니다.
             </DialogDescription>
           </DialogHeader>
           <Form {...form}>
@@ -98,13 +98,13 @@ export const InitialModal = () => {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                        Server name
+                        서버 이름
                       </FormLabel>
                       <FormControl>
                         <Input
                           disabled={isLoading}
                           className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                          placeholder="Enter server name"
+                          placeholder="서버 이름을 입력하세요."
                           {...field}
                         />
                       </FormControl>
@@ -115,7 +115,7 @@ export const InitialModal = () => {
               </div>
               <DialogFooter className="bg-gray-100 px-6 py-4">
                 <Button disabled={isLoading} variant="primary">
-                  Create
+                  생성
                 </Button>
               </DialogFooter>
             </form>
