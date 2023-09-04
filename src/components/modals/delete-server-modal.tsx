b278d90e1b0eec22594ce4fa3356a93ccc1338d9
BearHumanS <bye0651@naver.com>
2023-09-05 02:33:45 +0900

    FEAT: 관리자 서버 삭제, 게스트 서버 나가기 기능

---
+++ src/components/modals/delete-server-modal.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export const DeleteServerModal = () => {
+  const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "deleteServer";
+  const { server } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/servers/${server?.id}`);
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <Dialog open={isModalOpen} onOpenChange={onClose}>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl text-center font-bold">
+              서버 삭제하기
+            </DialogTitle>
+            <DialogDescription className="text-center text-zinc-500">
+              서버를 삭제하시겠습니까? <br />
+              <span className="font-semibold text-indigo-500">
+                {server?.name}
+              </span>
+              는 영구적으로 삭제됩니다.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="bg-gray-100 px-6 py-4">
+            <div className="flex items-center justify-between w-full">
+              <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                취소
+              </Button>
+              <Button disabled={isLoading} onClick={onClick} variant="primary">
+                확인
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
