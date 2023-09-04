4e65eaa572efcdaccea6b8f8a5dacdf5cb5deb59
BearHumanS <bye0651@naver.com>
2023-09-04 20:47:03 +0900

    FEAT: 서버 업데이트 기능

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -42,18 +42,21 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           <ChevronDown className="h-5 w-5 ml-auto" />
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent
-        onClick={() => onOpen("invite", { server })}
-        className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
-      >
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 pt-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 pt-2 text-sm cursor-pointer"
+          >
             초대하기
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 pt-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className="px-3 pt-2 text-sm cursor-pointer"
+          >
             설정
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
